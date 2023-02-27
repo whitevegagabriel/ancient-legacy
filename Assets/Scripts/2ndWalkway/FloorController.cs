9b4ca03e70fed3f14886ff351db09b06a6357d73
Yipeng Xue <yipeng.xue@outlook.com>
2023-02-26 19:07:18 -0500

    falling platform first commit

---
+++ Assets/Scripts/2ndWalkway/FloorController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour
+{
+    bool isFalling = false;
+    float downSpeed = 0;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //can add a check first 
+        //if (other.gameObject.name == "player")
+        isFalling = true;
+    }
+
+    void Update()
+    {
+        if (isFalling)
+        {
+            downSpeed += Time.deltaTime/5;
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y - downSpeed,
+                transform.position.z);
+        }
+
+    }
+}
