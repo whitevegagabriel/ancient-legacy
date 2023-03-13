4c8fbf21d6855c61e674d07d505ac6054efddf66
Lawrence Yim <lawrence.yim.010@gmail.com>
2023-03-13 17:55:46 -0400

    Added a victory screen

---
+++ Assets/Scripts/VictoryMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+[RequireComponent(typeof(CanvasGroup))] 
+public class VictoryMenu : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    void Start()
+    {
+        Time.timeScale = 1f;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0f;
+    }
+
+    public void DisplayOnCollection() {
+        Time.timeScale = 0f;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
