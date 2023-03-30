c399e0a8808082bec568a4bb54d07b685b32b343
Gabriel White-Vega <whitevegagabriel@gmail.com>
2023-03-29 20:42:54 -0400

    Revert "Pr98" (#101)

--- Assets/Scripts/AI/OrbAI.cs
+++ Assets/Scripts/AI/OrbAI.cs
@@ -1,13 +1,7 @@
-using System;
 using System.Collections;
-using System.Linq;
-using CleverCrow.Fluid.BTs.Tasks;
-using CleverCrow.Fluid.BTs.Tasks.Actions;
-using CleverCrow.Fluid.BTs.Trees;
 using Combat;
 using UnityEngine;
 using UnityEngine.AI;
-using Random = UnityEngine.Random;
 
 namespace AI
 {
@@ -15,21 +9,30 @@ namespace AI
     {
         public GameObject[] waypoints;
 
-        [SerializeField] private BehaviorTree tree;
-
-        private const float ChaseDistance = 7f;
+        private float _chaseDistance = 7f;
+        private State _state;
         private NavMeshAgent _agent;
         private GameObject _player;
         private WeaponController _weaponController;
         private Targetable _targetable;
         private Animator _animator;
         private static readonly int HoveringOffset = Animator.StringToHash("HoveringOffset");
+        private static readonly int HoveringSpeed = Animator.StringToHash("HoveringSpeed");
         private float _hoveringSpeed;
-        private int _currentWaypoint;
-        
-        private void Awake()
+
+        private enum State
+        {
+            Patrol,
+            Chase,
+            Knockback,
+            None,
+        }
+
+        // Start is called before the first frame update
+        void Start()
         {
             _agent = GetComponent<NavMeshAgent>();
+            _player = GameObject.FindGameObjectWithTag("Player");
             _weaponController = GetComponentInChildren<WeaponController>();
             _weaponController.SetDamage(1);
             _targetable = GetComponent<Targetable>();
@@ -38,165 +41,112 @@ namespace AI
             // a random offset to make the orbs look more independent
             _animator.SetFloat(HoveringOffset, Random.Range(0f, 1f));
             _hoveringSpeed = Random.Range(0.8f, 1.2f);
-            
-            tree = new BehaviorTreeBuilder(gameObject)
-                .Selector()
-                    // Die
-                    .Sequence()
-                        .Condition(() => _targetable.GetHealth() <= 0)
-                        .Do(() =>
-                        {
-                            EventManager.TriggerEvent<AIAudioHandler.OrbDeathEvent>();
-                            Destroy(gameObject);
-                            return TaskStatus.Success;
-                        })
-                    .End()
-                    // Knock back
-                    .Sequence()
-                        .Condition(() =>
-                        {
-                            return Physics.OverlapSphere(transform.position, .3f)
-                                .Any(c => c.CompareTag("Player"));
-                        })
-                        .Sequence()
-                            .Do(() =>
-                            {
-                                EventManager.TriggerEvent<AIAudioHandler.OrbAttackEvent>();
-                                
-                                // move agent in opposite direction of player
-                                var playerToSelf = transform.position - _player.transform.position;
-                                GetComponent<Rigidbody>().isKinematic = false;
-                                GetComponent<Rigidbody>().AddForce(playerToSelf.normalized * 3f, ForceMode.Impulse);
-                                
-                                return TaskStatus.Success;
-                            })
-                            .WaitTime(1.1f)
-                            .Do(() =>
-                            {
-                                GetComponent<Rigidbody>().isKinematic = true;
-                                return TaskStatus.Success;
-                            })
-                        .End()
-                    .End()
-                    // Do nothing if path pending
-                    .Sequence()
-                        .Condition(() => _agent.pathPending)
-                        .Do(() => TaskStatus.Success)
-                    .End()
-                    // Chase player
-                    .Sequence()
-                        .Condition(() => Vector3.Distance(_agent.transform.position, _player.transform.position) < ChaseDistance)
-                        .ChasePlayer(_hoveringSpeed)
-                    .End()
-                    // Patrol
-                    .Patrol(waypoints, _hoveringSpeed)
-                .End()
-                .Build();
+            SetState(State.Patrol);
         }
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            _player = GameObject.FindGameObjectWithTag("Player");
-        }
-
+        // Update is called once per frame
         void Update()
         {
-            tree.Tick();
-        }
-    }
-    
-    public class ChasePlayerAction : ActionBase
-    {
-        private static readonly int HoveringSpeed = Animator.StringToHash("HoveringSpeed");
-        
-        private NavMeshAgent _agent;
-        private GameObject _player;
-        private Animator _animator;
-        private WeaponController _weaponController;
-        
-        public float hoveringSpeed;
-        
-        protected override void OnInit () 
-        {
-            _agent = Owner.GetComponent<NavMeshAgent>();
-            _player = GameObject.FindGameObjectWithTag("Player");
-            _animator = Owner.GetComponent<Animator>();
-            _weaponController = Owner.GetComponentInChildren<WeaponController>();
+            if (_agent.pathPending) return;
+
+            if (_targetable.GetHealth() <= 0)
+            {
+                EventManager.TriggerEvent<AIAudioHandler.OrbDeathEvent>();
+                Destroy(gameObject);
+                return;
+            }
+
+            switch (_state)
+            {
+                case State.Patrol:
+                    HandlePatrol();
+                    break;
+                case State.Chase:
+                    HandleChase();
+                    break;
+                case State.Knockback:
+                    HandleKnockback();
+                    break;
+            }
         }
 
-        protected override void OnStart () {
-            _animator.SetFloat(HoveringSpeed, hoveringSpeed * 1.4f);
-            _weaponController.StartAttack();
+        private void HandlePatrol()
+        {
+            if (Vector3.Distance(_agent.transform.position, _player.transform.position) < _chaseDistance)
+            {
+                SetState(State.Chase);
+                return;
+            }
+
+            if (waypoints.Length == 0)
+            {
+                Debug.LogError("No waypoints found");
+                return;
+            }
+
+            if (_agent.remainingDistance > 0.5f) return;
+            
+            // set random waypoint
+            var currentWaypoint = Random.Range(0, waypoints.Length);
+            var waypoint = waypoints[currentWaypoint];
+            _agent.SetDestination(waypoint.transform.position);
         }
 
-        protected override TaskStatus OnUpdate()
+        private void HandleChase()
         {
+            if (Vector3.Distance(_agent.transform.position, _player.transform.position) > _chaseDistance)
+            {
+                SetState(State.Patrol);
+                return;
+            }
+
             _agent.SetDestination(_player.transform.position);
-            return TaskStatus.Success;
-        }
-    }
-
-    public class PatrolAction : ActionBase
-    {
-        private static readonly int HoveringSpeed = Animator.StringToHash("HoveringSpeed");
-        
-        private NavMeshAgent _agent;
-        private Animator _animator;
-        private Transform _currentWaypointTransform;
-        
-        public GameObject[] waypoints;
-        public float hoveringSpeed;
-        
-        protected override void OnInit () 
-        {
-            _agent = Owner.GetComponent<NavMeshAgent>();
-            _animator = Owner.GetComponent<Animator>();
-            
-            if (waypoints.Length > 0)
-            {
-                _currentWaypointTransform = waypoints[Random.Range(0, waypoints.Length)].transform;
-            }
-        }
-        
-        protected override void OnStart () 
-        {
-            _animator.SetFloat(HoveringSpeed, hoveringSpeed);
-            
-            if (_currentWaypointTransform != null)
-            {
-                _agent.SetDestination(_currentWaypointTransform.position);
-            }
-            else
-            {
-                _agent.ResetPath();
-            }
-        }
-        
-        protected override TaskStatus OnUpdate()
-        {
-            if (_agent.remainingDistance > 0.5f || waypoints.Length == 0)
-            {
-                return TaskStatus.Success;
-            }
-            
-            _currentWaypointTransform = waypoints[Random.Range(0, waypoints.Length)].transform;
-            _agent.SetDestination(_currentWaypointTransform.position);
-            return TaskStatus.Success;
-        }
-    }
-    
-    public static class BehaviorTreeBuilderExtensions
-    {
-        public static BehaviorTreeBuilder ChasePlayer(this BehaviorTreeBuilder builder, float hoveringSpeed)
-        {
-            return builder.AddNode(new ChasePlayerAction { hoveringSpeed = hoveringSpeed });
         }
 
-        public static BehaviorTreeBuilder Patrol(this BehaviorTreeBuilder builder, GameObject[] waypoints,
-            float hoveringSpeed)
+        private void HandleKnockback()
         {
-            return builder.AddNode(new PatrolAction { waypoints = waypoints, hoveringSpeed = hoveringSpeed });
+            EventManager.TriggerEvent<AIAudioHandler.OrbAttackEvent>();
+            StartCoroutine(Knockback());
+            SetState(State.None);
+        }
+
+        private IEnumerator Knockback()
+        {
+            // move agent in opposite direction of player
+            var playerToSelf = transform.position - _player.transform.position;
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().AddForce(playerToSelf.normalized * 3f, ForceMode.Impulse);
+            yield return new WaitForSeconds(1f);
+            GetComponent<Rigidbody>().isKinematic = true;
+            SetState(State.Patrol);
+        }
+
+        private void SetState(State state)
+        {
+            switch (state)
+            {
+                case State.Patrol:
+                    _animator.SetFloat(HoveringSpeed, _hoveringSpeed);
+                    break;
+                case State.Chase:
+                    _animator.SetFloat(HoveringSpeed, _hoveringSpeed * 1.4f);
+
+                    _weaponController.StartAttack();
+                    break;
+                case State.Knockback:
+                    break;
+            }
+
+            _state = state;
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player") && _state == State.Chase)
+            {
+                Debug.Log("knockback");
+                SetState(State.Knockback);
+            }
         }
     }
 }
