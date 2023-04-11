62664a0f97bec550a72378493de3ea27a4e50e3c
Gabriel White-Vega <whitevegagabriel@gmail.com>
2023-04-10 21:33:45 -0400

    Speed up vertical platforms and have player move with it (#116)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -243,7 +243,7 @@ public class PlayerController : MonoBehaviour {
             var ground = groundPriority[0];
             var groundPosition = ground.position;
             var groundMovement = groundPosition - lastGroundPosition;
-            controller.Move(groundMovement);
+            transform.position += groundMovement;
             lastGroundPosition = groundPosition;
         } 
 
